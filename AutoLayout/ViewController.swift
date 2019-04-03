1c7ed02cd33726738dc6e7f4f7dc0305fcab5fac
Rinto Andrews <rintoandrews90@gmail.com>
2019-04-03 16:29:37 +0530

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Rinto Andrews on 03/04/19.
+//  Copyright © 2019 Rinto Andrews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
